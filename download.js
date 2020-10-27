842706b9bb2702b6a0162de924b7929cf8585173
Chris Mawdesley <chris.mawdesley@gluckgames.com>
2020-10-27 23:08:41 +0000

    escape spaces in file paths

--- download.js
+++ download.js
@@ -1,6 +1,7 @@
 const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
 const { promises: { writeFile, readFile },  existsSync } = require("fs");
+const escape = require("escape-path-with-spaces")
 
 const [server, sourcePath] = process.argv[2].split(":");
 const destinationPath = process.argv[3];
@@ -8,15 +9,15 @@ const ignoreFile = `${destinationPath}/.download-once-ignore`;
 
 const getIgnoreList = () => readFile(ignoreFile).then(ignoreList => ignoreList.toString().split("\n")).catch(() => []);
 const addToIgnoreList = file => writeFile(ignoreFile, `${file}\n`, { flag: "a" });
-const getFilesInSource = () => exec(`ssh ${server} "find ${sourcePath} -not -type d"`).then(({ stdout: files }) => files.toString().split("\n").filter(file => file.length));
+const getFilesInSource = () => exec(`ssh ${server} "find '${escape(sourcePath)}' -not -type d"`).then(({ stdout: files }) => files.toString().split("\n").filter(file => file.length));
 const getFilesToDownload = async () =>  {
     const [ignoreList, filesInSource] = await Promise.all([getIgnoreList(), getFilesInSource()]);
     return filesInSource.filter(file => !ignoreList.includes(file));
 }
 const getFolder = file => file.substr(0, file.lastIndexOf("/"));
 const translateToDestination = file => file.replace(sourcePath, destinationPath);
-const createDestinationFolder = file => exec(`mkdir -p ${translateToDestination(getFolder(file))}`);
-const download = file => exec(`scp ${server}:${file} ${translateToDestination(file)}`);
+const createDestinationFolder = file => exec(`mkdir -p '${escape(translateToDestination(getFolder(file)))}'`);
+const download = file => exec(`scp ${server}:'${escape(file)}' '${escape(translateToDestination(file))}'`);
 const downloadFiles = async files => {
     for (const file of files) {
         try {
